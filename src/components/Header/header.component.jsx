a2b34ada2ed55fc18efcf2aa485c6dacd54ef347
Antonio Carlos <antonio.oconner@gmail.com>
2022-12-30 19:18:04 -0300

    test

---
+++ src/components/Header/header.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Wrapper } from "./header.styles";
+import { Title } from './header.styles';
+
+const Header = () => {
+  return (
+    <Wrapper>
+        <Title>
+          Rick and Morty
+        </Title>       
+    </Wrapper>
+  );
+}
+
+export default Header;
