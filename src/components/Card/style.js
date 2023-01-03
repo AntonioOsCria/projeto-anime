1804168f19048560187f87a9f14ed4ab48a26312
Antonio Carlos <antonio.oconner@gmail.com>
2023-01-03 14:27:23 -0300

    color change

--- src/components/Card/style.js
+++ src/components/Card/style.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const CardStyle = styled.div`
- background: orange;
+ background: #66BEAB;
  width: 230px;
  padding: 15px; 
  border-radius: 25px;
--- src/styles/global.js
+++ src/styles/global.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export const Container = styled.div`
     width: 100vw;
     height: 100vh;
-    background: yellow;
+    background: Grey;
     background-attachment: fixed;
     display: flex;
     flex-direction: column;
