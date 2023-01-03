a09b263dddc8a9872a6ea0e361952f455e482b7c
Antonio Carlos <antonio.oconner@gmail.com>
2023-01-03 16:28:51 -0300

    status

--- src/pages/main/main.page.jsx
+++ src/pages/main/main.page.jsx
@@ -5,7 +5,7 @@ import { Card } from '../../components/Card/card';
 export const MainPage = () => {
   return (
     <Container>
-      <Card title="Title ind Card" />
+      <Card title="Title ind Card" information={"Test"}/>
     </Container>
    
 );
